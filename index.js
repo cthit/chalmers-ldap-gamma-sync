1cc35f28a2e369c033552a0e7e900672a5ae9800
Goostaf <gasplund2@gmail.com>
2023-08-23 18:32:58 +0200

    Use allSettled, cleaner logging

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const axios = require("axios");
 const ldap = require("ldapjs");
 
+const COLOR_RESET = "\x1b[0m";
+const FG_YELLOW = "\x1b[33m";
+const BG_RED = "\x1b[41m";
+
 const client = ldap.createClient({
 	url: "ldap://ldap.chalmers.se",
 });
 
 (async function () {
-	console.log("Syncing...");
-	let cids = await new Promise((resolve, _) => {
+	console.log("Sync started");
+	const cids = await new Promise((resolve, _) => {
+		console.log("Connecting to LDAP...");
 		client.search(
 			"ou=groups,dc=chalmers,dc=se",
 			{
@@ -32,30 +37,37 @@ const client = ldap.createClient({
 		process.exit(0);
 	}
 
-	console.log("Found", cids.length, "CIDs");
+	console.log("The following", cids.length, "CIDs were found and will be added:");
 	console.dir(cids, {depth: null, colors: true, maxArrayLength: null});
+	console.log("\nAdding CIDs...");
 
 	// Lägg till dem en och en eftersom att det krashar annars
-	await Promise.all(
+	const results = await Promise.allSettled(
 		cids.map(async (cid) => {
-			try {
-				await axios({
-					method: "POST",
-					url: "https://gamma.chalmers.it/api/admin/users/whitelist",
-					data: {
-						cids: [cid],
-					},
-					headers: {
-						Authorization: "pre-shared " + process.env.GAMMA_API_KEY,
-						"Content-Type": "application/json",
-					},
-				});
-				console.log("Added:", cid);
-			} catch (e) {
-				console.error("Could not add", cid + ":", e.message);
-			}
+			await axios({
+				method: "POST",
+				url: "https://gamma.chalmers.it/api/admin/users/whitelist",
+				data: {
+					cids: [cid],
+				},
+				headers: {
+					Authorization: "pre-shared " + process.env.GAMMA_API_KEY,
+					"Content-Type": "application/json",
+				},
+			});
 		})
 	);
+
+	const cidResults = results.map((r, idx) => { return {result: r, cid: cids[idx]} });
+	const failed = cidResults.filter((r) => r.result.status === "rejected")
+								.map((r) => r.cid + ": " + FG_YELLOW + r.result.reason.message + COLOR_RESET);
+
+	console.log(cids.length - failed.length, "added,", failed.length, "failed out of", cids.length, "total");
+
+	if (failed.length > 0) {
+		console.warn("\n" + BG_RED +"The following CIDs failed to add:" + COLOR_RESET);
+		failed.forEach((f) => console.warn(f));
+	}
 
 	process.exit(0);
 })();
